2985ba0f485fddfbbafd1f85d927588a3dc93642
Joerg Amelunxen <jamelunx@mail.uni-paderborn.de>
2017-12-16 14:14:54 +0100

    add test case framework

---
+++ src/tests/indicator/smaTest.spec.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const expect = require('chai').expect
+
+describe('SMA indicator behaviour', () => {
+
+  describe('"calculation"', () => {
+
+    it('add me', () => {
+      expect('true').to.be.a('string');
+    })
+
+  })
+
+})
